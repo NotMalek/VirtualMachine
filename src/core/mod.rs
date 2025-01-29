51203800cf14d2a0056ac632fad33647c5c2b92b
NotMalek <malekanes213@gmail.com>
2025-01-29 18:49:16 -0500

    Refactored backend files

--- src/core/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod assembler;
-pub mod error;
-pub mod heap;
-pub mod instruction;
-pub mod state;
-pub mod vm;
--- src/tests/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod io_test;
-pub mod array_test;
-pub mod string_test;
-pub mod arithmetic_test;
-pub mod control_test;
